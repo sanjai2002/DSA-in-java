7f8b7a26e9d1320c8f3bee6aaa069fb2562d38b6
sanjai2002 <sanjaihari2002@gmail.com>
2024-09-04 11:40:09 +0530

    Add: Sorting methods

--- Sorting/CycleSort.java
+++ Sorting/CycleSort.java
@@ -8,7 +8,26 @@ public class CycleSort {
 
     }
     public static void Cycle(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr,correct,i);
 
+            }
+            else{
+                i++;
+            }
+
+
+
+        }
+
+    }
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
     
 }
